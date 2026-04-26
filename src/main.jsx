5095899aec3eedee07c11b0bdfeb09b6e34601e0
Sudo-o-O <gibbsmatthew@outlook.com>
2026-04-26 17:05:58 -0400

    Fix router import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import "./default.css";
 
